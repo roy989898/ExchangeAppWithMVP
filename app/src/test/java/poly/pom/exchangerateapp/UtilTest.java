d5bb2486512dc15ee5873e02c4dece99253373de
roy989898 <roy989898@gmail.com>
2016-08-31 23:28:05 +0800

    chor:add the test to tes the getTodayDate

---
+++ app/src/test/java/poly/pom/exchangerateapp/UtilTest.java
@@ -0,0 +1,25 @@
+package poly.pom.exchangerateapp;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by User on 31/8/2016.
+ */
+public class UtilTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+
+    }
+
+    @Test
+    public void testGetTodayDate() throws Exception {
+        assertEquals("20160831",Util.getTodayDate());
+
+    }
+}
