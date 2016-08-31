42391e656926c262f255a60309d69dc6a459ab6c
roy989898 <roy989898@gmail.com>
2016-08-31 18:08:34 +0800

    refactor:chnage the retrofit test to the android test

--- app/src/androidTest/java/poly/pom/exchangerateapp/ApplicationTest.java
+++ app/src/androidTest/java/poly/pom/exchangerateapp/ApplicationTest.java
@@ -1,13 +1,52 @@
 package poly.pom.exchangerateapp;
 
 import android.app.Application;
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import poly.pom.exchangerateapp.repository.RetrofitModule.Bank;
+import poly.pom.exchangerateapp.repository.RetrofitModule.FixerIOAPI;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.GsonConverterFactory;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+
+    @Test
+    public void testName() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.fixer.io/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        FixerIOAPI fixerIOAPi = retrofit.create(FixerIOAPI.class);
+        Call<Bank> call = fixerIOAPi.loadLatestEeurBaseRate();
+
+        call.enqueue(new Callback<Bank>() {
+            @Override
+            public void onResponse(Response<Bank> response) {
+                Bank bank = response.body();
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+
+            }
+        });
     }
 }
