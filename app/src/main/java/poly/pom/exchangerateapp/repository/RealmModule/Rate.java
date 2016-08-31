16511632ed4a9fc2b9f5e6d8750524e3e34507fa
roy989898 <roy989898@gmail.com>
2016-08-31 15:36:26 +0800

    feat:crate Realm use class

---
+++ app/src/main/java/poly/pom/exchangerateapp/repository/RealmModule/Rate.java
@@ -0,0 +1,35 @@
+package poly.pom.exchangerateapp.repository.RealmModule;
+
+import io.realm.annotations.PrimaryKey;
+
+
+public class Rate {
+    @PrimaryKey
+    private String name;
+    private double rateBaseEur;
+    private int updateDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getRateBaseEur() {
+        return rateBaseEur;
+    }
+
+    public void setRateBaseEur(double rateBaseEur) {
+        this.rateBaseEur = rateBaseEur;
+    }
+
+    public int getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(int updateDate) {
+        this.updateDate = updateDate;
+    }
+}
