c3f70f3085dca10a49653b3ee6bf2edcaea086b2
roy989898 <roy989898@gmail.com>
2016-09-01 13:22:33 +0800

    feat:add column string

---
+++ app/src/main/java/poly/pom/exchangerateapp/repository/RealmColumnName.java
@@ -0,0 +1,10 @@
+package poly.pom.exchangerateapp.repository;
+
+/**
+ * Created by User on 1/9/2016.
+ */
+public class RealmColumnName {
+    static public String COL_NAME="name";
+    static public String COL_RATE="rateBaseEur";
+    static public String COL_UPDTATEDATE="updateDate";
+}
