2b588a60c717695432db59da5c31c887e53fc16c
roy989898 <roy989898@gmail.com>
2016-09-01 21:42:30 +0800

    refactor:make Retrofit use Rxjava

--- app/src/main/java/poly/pom/exchangerateapp/repository/RetrofitModule/FixerIOAPI.java
+++ app/src/main/java/poly/pom/exchangerateapp/repository/RetrofitModule/FixerIOAPI.java
@@ -2,10 +2,11 @@ package poly.pom.exchangerateapp.repository.RetrofitModule;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import rx.Observable;
 
 
 public interface FixerIOAPI {
 //   http://api.fixer.io/latest
     @GET("/latest")
-    Call<Bank> loadLatestEeurBaseRate();
+    Observable<Bank> loadLatestEeurBaseRate();
 }
